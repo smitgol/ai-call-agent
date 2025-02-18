15373ed0e02572bfa13f25042d2b5f7b971a9707
smitgol <smitgol007@gmail.com>
2025-02-18 22:32:08 +0530

    added requirements file

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from services.twilio import twilio_handler
 
 
 def main():
-	proxy_server = websockets.serve(twilio_handler, 'localhost', 5000)
+	proxy_server = websockets.serve(twilio_handler, '0.0.0.0', 5000)
 	print('Server starting on ws://localhost:5000')
 	asyncio.get_event_loop().run_until_complete(proxy_server)
 	asyncio.get_event_loop().run_forever()
