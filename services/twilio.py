46c210026e34d9e47579073e3a4b0112f5289d48
smitgol <smitgol007@gmail.com>
2025-05-13 22:58:10 +0530

    twilio file changed

--- services/twilio.py
+++ services/twilio.py
@@ -12,18 +12,10 @@ from utils import text_chunker, getAudioContent, get_twilio_client
 import websockets
 from services.config import initial_message, repeat_message
 import audioop
-import webrtcvad
 import logging
 import uuid
-import torch
-import numpy as np
 from .call_transcription import TranscriptionLogger
-from pydub import AudioSegment
 
-# Load the Silero VAD model and its utility functions.
-# This will download the model if it isn't already cached.
-model, utils = torch.hub.load('snakers4/silero-vad', 'silero_vad', force_reload=False)
-(get_speech_ts, _, _, _, _) = utils
 
 
 SAMPLING_RATE = 8000
@@ -31,99 +23,88 @@ SAMPLING_RATE = 8000
 logger = logging.getLogger(__name__)
 
 async def twilio_handler(client_ws):
-    outbox = asyncio.Queue()
-    audio_queue = asyncio.Queue()
     #streamsid_queue = asyncio.Queue()
+    outbox = asyncio.Queue()
+    websocket_connected_time = time.time()
+    print("websocket connected time first time: ", time.time() - websocket_connected_time)
 
     stt_start_time = 0.0  # Initialize here so both functions can access it
     llm_start_time = 0.0
     tts_start_time = 0.0
+    stt_processing_time = 0.0
 
     stream_service = StreamService(client_ws)
     #stt_service = STTService("twilio")
-    stt_service = await openai_stt_connect()
-    await stt_service.send(json.dumps(openai_ws_config()))
-    #await stt_service.connect()
+    #stt_service = await openai_stt_connect()
+    stt_service = None
+    stt_receiver_task = None
+    #await stt_service.send(json.dumps(openai_ws_config()))
 
-    llm_service = LLMService()
-    tts_service = TTSService("twilio")
-    await tts_service.connect_tts()
+    #llm_service = LLMService()
+    llm_service = None
+    #tts_service = TTSService("twilio")
+    tts_service = None
+    #await tts_service.connect_tts()
     marks = deque()
     tts_task = None
     end_call_after_this_mark = None
-    audio_buffer = np.array([], dtype=np.float32)
     audio_cursor = 0.
+
 
     #var for log
     transcription_logger = None
     try:
-
         ## Region STT 
         async def stt_sender(deepgram_ws):
-            print('started deepgram sender')
-            while True:
-                chunk = await outbox.get()
-                await deepgram_ws.send(chunk)
-                print('sent audio to deepgram')
+             while True:
+                 chunk = await outbox.get()
+                 await deepgram_ws.send(chunk)
 
         async def stt_receiver(deepgram_ws):
-            nonlocal audio_cursor
+            print('started deepgram receiver')
+            nonlocal audio_cursor, stt_start_time, stt_processing_time
             async for message in deepgram_ws:
                 try:
                     dg_json = json.loads(message)
-                    print(dg_json)
-                    # print the results from deepgram!
+                    # print the results from openai!
+                    
                     if dg_json['type'] == "conversation.item.input_audio_transcription.completed":
                         transcription = dg_json['transcript']
                         if transcription.strip():
                             if transcription_logger:
+                                print("stt time: ", time.time() - stt_start_time)
+                                print("stt processing time: ", time.time() - stt_processing_time)
+                                print("user timing for stt: ",  stt_start_time - stt_processing_time)
                                 transcription_logger.add_entry("STT", transcription)
                             await llm_service.complete_with_chunks(transcription)
                     elif dg_json['type'] == "input_audio_buffer.speech_started":
+                        stt_start_time = time.time()
                         print('speech started')
+                        logger.info("speech started")
                         await stop_speaking()
+                    elif dg_json['type'] == "input_audio_buffer.speech_ended":
+                        print('speech ended')
+                        logger.info("speech ended")
+                        stt_processing_time = time.time()
                     '''
                     if dg_json["is_final"] == True:
-                        transcript = dg_json["channel"]["alternatives"][0]["transcript"]
-                        if transcript.strip():
-                            if transcription_logger:
-                                transcription_logger.add_entry("STT", transcript)
-                                #logger.info("stt transcript: ", transcript)
-                            print('final transcript: ' + transcript)
-                            await llm_service.complete_with_chunks(transcript)
+                         transcript = dg_json["channel"]["alternatives"][0]["transcript"]
+                         if transcript.strip():
+                             if transcription_logger:
+                                 transcription_logger.add_entry("STT", transcript)
+                                 #logger.info("stt transcript: ", transcript)
+                             print('final transcript: ' + transcript)
+                             print("stt time: ", time.time() - stt_start_time)
+                             await llm_service.complete_with_chunks(transcript)
+                    elif dg_json["type"] == "SpeechStarted":
+                        stt_start_time = time.time()
+                        print('speech started')
+                        #await stop_speaking()
                     '''
                 except:
                     print('was not able to parse deepgram response as json')
                     continue
             print('finished deepgram receiver')
-        async def process_media(msg):
-            nonlocal audio_buffer
-            raw_data = base64.b64decode(msg['media']['payload'])
-            #stt_service.stream(raw_data)
-            pcm_data = audioop.ulaw2lin(raw_data, 2)
-    
-            # Convert and normalize
-            chunk_np = np.frombuffer(pcm_data, dtype=np.int16).astype(np.float32) / 32768.0
-            audio_buffer = np.concatenate((audio_buffer, chunk_np))
-            # Process every 500ms (adjust based on your needs)
-            if len(audio_buffer) >= SAMPLING_RATE * 0.5:  # 500ms of audio
-                try:
-                    speech_segments = get_speech_ts(
-                        audio_buffer,
-                        model,
-                        sampling_rate=SAMPLING_RATE,
-                        threshold=0.4,  # Lowered threshold
-                        min_speech_duration_ms=300  # Adjust as needed
-                    )
-                    if speech_segments:
-                        logger.info("Speech detected")
-                        await stop_speaking()
-                    # Reset buffer after processing
-                    audio_buffer = np.array([], dtype=np.float32)
-                except Exception as e:
-                    logger.error(f"VAD error: {e}")
-
-
 
         async def handle_audio_sent(mark_label):
             marks.append(mark_label)
@@ -161,9 +142,9 @@ async def twilio_handler(client_ws):
             if transcription_logger:
                     transcription_logger.add_entry("LLM", "Speaking default text")
             await tts_service.get_audio(repeat_message)
-
         # END STT Region
 
+        # LLM PART
         async def handle_llm(transcript):
             nonlocal llm_start_time
             stt_total_time = time.time() - stt_start_time
@@ -183,6 +164,16 @@ async def twilio_handler(client_ws):
             except Exception as e:
                 logger.error(f"Error handling tool call: {e}")
         
+        async def handle_llm_cache_stream(response):
+            try:
+                if transcription_logger:
+                    transcription_logger.add_entry("LLM", response)
+                    logger.info("added to transcription log")
+                await tts_service.get_audio(response)
+            except Exception as e:
+                logger.error(f"Error sending chunks to TTS: {e}")
+        # LLM PART END
+
         # TO BE USED WHEN USING TTS BY API
         # async def handle_tts(text):
         #     try:
@@ -198,9 +189,7 @@ async def twilio_handler(client_ws):
         # TTS PART
         async def send_chunks_to_tts(text_iterator):
             try:
-                nonlocal tts_task
-                nonlocal llm_start_time
-                nonlocal tts_start_time
+                nonlocal tts_task, llm_start_time, tts_start_time
                 if tts_task:
                     tts_task.cancel()
 
@@ -208,6 +197,7 @@ async def twilio_handler(client_ws):
                 await tts_service.connect_tts()
                 tts_task = asyncio.create_task(send_audio_chunks_to_twilio(tts_listener()))
                 complete_sentence = ""
+                logger.info("FIRST CHUNK RECEIVED")
                 async for text in text_chunker(text_iterator, llm_service):
                     complete_sentence += text
                     if text:  # Skip empty strings
@@ -216,20 +206,18 @@ async def twilio_handler(client_ws):
                             logger.info(f"Sent text to TTS: {text}")
                         except Exception as e:
                             logger.error(f"Error sending text to TTS: {e} {text}")
-                            logger.error("TTS WebSocket closed, reconnecting...")
                             tts_task.cancel()
                             await tts_service.connect_tts()
                             await tts_service.tts_ws.send(json.dumps({"text": text}))
                 llm_service.user_context.append({"role": "assistant", "content": complete_sentence}) #pushing the complete sentence to user context
-                logger.info(f"Complete sentence from llm: {complete_sentence}")
-                if transcription_logger:
-                    transcription_logger.add_entry("LLM", complete_sentence)
-                    logger.info("added to transcription log")
-                total_llm_time = time.time() - llm_start_time
-                logger.info(f"LLM processing time: {total_llm_time:.2f} seconds")
                 if tts_service.tts_ws:
                     await tts_service.end_tts_streaming(tts_service.tts_ws)
                 await tts_task
+
+                if transcription_logger:
+                    transcription_logger.add_entry("LLM", complete_sentence)
+                total_llm_time = time.time() - llm_start_time
+                logger.info(f"LLM COMPLETED, LLM processing time: {total_llm_time:.2f} seconds")
                 tts_start_time = time.time()
             except Exception as e:
                 logger.error(f"Error sending chunks to TTS: {e}")
@@ -252,7 +240,6 @@ async def twilio_handler(client_ws):
             logger.info(f"TTS processing time: {total_tts_time:.2f} seconds")
 
 
-        
         async def tts_listener():
             ws = tts_service.tts_ws
             nonlocal tts_task
@@ -307,7 +294,7 @@ async def twilio_handler(client_ws):
             logger.info("Client receiver connected")
             nonlocal transcription_logger
             nonlocal audio_cursor
-            BUFFER_SIZE = 20 * 160
+            BUFFER_SIZE = 1600 # 200ms of audio at 8000Hz
             buffer = bytearray(b'')
             empty_byte_received = False
             while True:
@@ -327,19 +314,12 @@ async def twilio_handler(client_ws):
                         await stream_service.sent_audio(audio_content)
                     elif event_type == "media":
                         media = message['media']
-                        chunk = base64.b64decode(media['payload'])
-                        audio_append = {
-                            "type": "input_audio_buffer.append",
-                            "audio": message['media']['payload']
-                        }
-                        if stt_service:
-                            await stt_service.send(json.dumps(audio_append))
-                        #asyncio.create_task(process_media(message))                    
-                        #time_increment = len(chunk) / 8000.0
-                        #audio_cursor += time_increment
-                        #buffer.extend(chunk)
-                        #if chunk == b'':
-                        #    empty_byte_received = True
+                        chunk = base64.b64decode(media['payload'])                 
+                        time_increment = len(chunk) / 8000.0
+                        audio_cursor += time_increment
+                        buffer.extend(chunk)
+                        if chunk == b'':
+                            empty_byte_received = True
                     elif event_type == "mark":
                         mark_label = message['mark']['name']
                         if end_call_after_this_mark and mark_label == end_call_after_this_mark:
@@ -354,25 +334,41 @@ async def twilio_handler(client_ws):
                         break
                     if len(buffer) >= BUFFER_SIZE or empty_byte_received:
                         outbox.put_nowait(buffer)
+                        audio_append = {
+                            "type": "input_audio_buffer.append",
+                            "audio": base64.b64encode(buffer).decode()
+                        }
+                        if stt_service:
+                            await stt_service.send(json.dumps(audio_append))
                         buffer = bytearray(b'')
                 except Exception as e:
                     logger.error(f"Client Receiver Error: {e}")
-        
-        #stt_service.on("transcription", handle_llm)
-        #stt_service.on('utterance', handle_utterance)
-        #llm_service.on("llm_response", handle_tts) #only use when we using llm by api
-        llm_service.on("llm_stream", send_chunks_to_tts)
-        llm_service.on("tool_triggered", handle_tool_call)
-        tts_service.on("audio", send_audio_to_twilio) #only when we using tts by api
-        stream_service.on('audiosent', handle_audio_sent)
+                    
+        async def connect_to_services():
+            nonlocal stt_service, llm_service, tts_service, stt_receiver_task
+            stt_service = await openai_stt_connect()
+            await stt_service.send(json.dumps(openai_ws_config()))
+            llm_service = LLMService()
+            tts_service = TTSService("twilio")
+            await tts_service.connect_tts()
+            stt_receiver_task = asyncio.create_task(stt_receiver(stt_service))
+            #deepgram_sender_task = asyncio.create_task(stt_sender(stt_service))
+            #stt_service.on("transcription", handle_llm)
+            #stt_service.on('utterance', handle_utterance)
+            #llm_service.on("llm_response", handle_tts) #only use when we using llm by api
+            llm_service.on("llm_stream", send_chunks_to_tts)
+            llm_service.on("tool_triggered", handle_tool_call)
+            #llm_service.on("llm_cache_stream", handle_llm_cache_stream)
+            tts_service.on("audio", send_audio_to_twilio) #only when we using tts by api
+            stream_service.on('audiosent', handle_audio_sent)
 
 
         listener_task = asyncio.create_task(websocket_listener())
         processor_task = asyncio.create_task(message_processor())
-        #deepgram_task = asyncio.create_task(stt_sender(stt_service))
-        deepgram_receiver_task = asyncio.create_task(stt_receiver(stt_service))
+        connect_task = asyncio.create_task(connect_to_services())
+        #deepgram_sender_task = asyncio.create_task(stt_sender(stt_service))
 
-        await asyncio.gather(listener_task, processor_task, deepgram_receiver_task)
+        await asyncio.gather(listener_task, processor_task, connect_task)
 
     except Exception as e:
         logger.error(f"WebSocket Handler Error: {e}")
